078afea554c35b9fe72a9660bf09c91bc5bf18ba
Duc Anh <taphamducanh@gmail.com>
2023-09-04 16:21:26 +0700

    Update 30

--- src/components/WordGenerator-N4.vue
+++ src/components/WordGenerator-N4.vue
@@ -1,22 +1,24 @@
 <template>
-<div style="text-align: center">
-    <div v-if="currentWord">
-        <p style="font-size: 25px"><strong>Số từ còn lại: {{ remainWord }} </strong></p>
-        <p style="font-size: 25px" v-if="currentWord.kanji"><strong>{{ currentWord.kanji }}</strong></p>
-        <p style="font-size: 25px"><strong>{{ currentWord.japanese }}</strong></p>
-        <input @keyup.enter="focusOnVietnameseInput" v-model="romanjiInput" placeholder="Nhập Romanji" required class="input" size="50" ref="romanjiInput" trim />
-        <br />
-        <input @keyup.enter="checkInput" v-model="meaning" placeholder="Nhập nghĩa" required class="input" size="50" ref="vietnameseInput" trim />
-        <p>{{ hint }}</p>
-        <br />
-        <button @click="checkInput" class="button">Tiếp theo</button>
-        <button @click="giveHint" class="button" style="margin-left: 5px">Gợi ý</button>
+    <div style="text-align: center">
+        <div v-if="currentWord">
+            <p style="font-size: 25px"><strong>Số từ còn lại: {{ remainWord }} </strong></p>
+            <p style="font-size: 25px" v-if="currentWord.kanji"><strong>{{ currentWord.kanji }}</strong></p>
+            <p style="font-size: 25px"><strong>{{ currentWord.japanese }}</strong></p>
+            <input @keyup.enter="focusOnVietnameseInput" v-model="romanjiInput" placeholder="Nhập Romanji" required
+                class="input" size="50" ref="romanjiInput" trim />
+            <br />
+            <input @keyup.enter="checkInput" v-model="meaning" placeholder="Nhập nghĩa" required class="input" size="50"
+                ref="vietnameseInput" trim />
+            <p>{{ hint }}</p>
+            <br />
+            <button @click="checkInput" class="button">Tiếp theo</button>
+            <button @click="giveHint" class="button" style="margin-left: 5px">Gợi ý</button>
+        </div>
+        <div v-else>
+            <button @click="startExercise" class="button">Bắt đầu</button>
+        </div>
+        <div style="color: red; font-size: 20px">{{ error }}</div>
     </div>
-    <div v-else>
-        <button @click="startExercise" class="button">Bắt đầu</button>
-    </div>
-    <div style="color: red; font-size: 20px">{{ error }}</div>
-</div>
 </template>
 
 <script>
@@ -24,7 +26,8 @@
 // import words_26 from '../Data/N4/26.json';
 // import words_27 from '../Data/N4/27.json';
 // import words_28 from '../Data/N4/28.json';
-import words_29 from '../Data/N4/29.json';
+// import words_29 from '../Data/N4/29.json';
+import words_30 from '../Data/N4/30.json'
 
 export default {
     data() {
@@ -41,8 +44,8 @@ export default {
         };
     },
     created() {
-        this.words = words_29 //Ktra từ vựng 1 bài
-        // this.words = this.words.concat(words_26, words_27) //Ktra từ vựng nhiều bài
+        this.words = words_30 //Ktra từ vựng 1 bài
+        // this.words = this.words.concat(words_28, words_29) //Ktra từ vựng nhiều bài
     },
     methods: {
         focusOnVietnameseInput() {
