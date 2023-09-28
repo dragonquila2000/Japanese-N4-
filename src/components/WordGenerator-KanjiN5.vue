4e3b1ddb24fcbcbf40b4ceeda85274266adb0b23
anhtpd <anhtpd@runsystem.net>
2023-09-28 13:42:41 +0700

    Update kanji + N5

--- src/components/WordGenerator-KanjiN5.vue
+++ src/components/WordGenerator-KanjiN5.vue
@@ -3,14 +3,14 @@
         <div v-if="currentWord">
             <p style="font-size: 25px"><strong>Số từ còn lại: {{ remainWord }} </strong></p>
             <p style="font-size: 25px" v-if="currentWord.kanji"><strong>{{ currentWord.kanji }}</strong></p>
-            <p style="font-size: 25px"><strong>{{ currentWord.japanese }}</strong></p>
+            <input @keyup.enter="focusOnRomanjiInput" v-model="chineseInput" placeholder="Nhập Nghĩa Hán Tự" required class="input" size="50"/>
+            <br />
             <input @keyup.enter="focusOnVietnameseInput" v-model="romanjiInput" placeholder="Nhập Romanji" required class="input" size="50" ref="romanjiInput" />
             <br />
             <input @keyup.enter="checkInput" v-model="meaning" placeholder="Nhập nghĩa" required class="input" size="50" ref="vietnameseInput" />
             <p>{{ hint }}</p>
             <br />
             <button @click="checkInput" class="button">Tiếp theo</button>
-            <button @click="giveHint" class="button" style="margin-left: 5px">Gợi ý</button>
         </div>
         <div v-else>
             <button @click="startExercise" class="button">Bắt đầu</button>
@@ -20,7 +20,7 @@
     </template>
     
     <script>
-    // import N5_1 from '../Data/N5/N5_1.json'
+    import words_1 from '../Data/Kanji/N5/1.json'
     export default {
         data() {
             return {
@@ -28,17 +28,22 @@
                 usedIndices: [],
                 currentWordIndex: null,
                 currentWord: null,
+                chineseInput: '',
                 romanjiInput: '',
                 meaning: '',
                 error: '',
                 remainWord: null,
-                hint: '',
             };
         },
-        // created() {
-        //     this.words = N5_1;
-        // },
+        created() {
+            this.words = words_1;
+        },
         methods: {
+            focusOnRomanjiInput() {
+                if (this.$refs.romanjiInput) {
+                    this.$refs.romanjiInput.focus()
+                }
+            },
             focusOnVietnameseInput() {
                 if (this.$refs.vietnameseInput) {
                     this.$refs.vietnameseInput.focus();
@@ -50,14 +55,15 @@
                 this.remainWord = this.words.length
             },
             nextWord() {
-                if (this.$refs.romanjiInput) {
-                    this.$refs.romanjiInput.focus();
+                if (this.$refs.chineseInput) {
+                    this.$refs.chineseInput.focus();
                 }
                 if (this.usedIndices.length === this.words.length) {
                     alert('Xuất sắc! Bạn đã biết thêm nhiều từ tiếng Nhật mới');
                     this.usedIndices = [];
                     this.currentWordIndex = null;
                     this.currentWord = null;
+                    this.chineseInput = ''
                     this.romanjiInput = '';
                     this.meaning = ''
                     return;
@@ -66,23 +72,20 @@
                     this.currentWordIndex = Math.floor(Math.random() * this.words.length);
                 } while (this.usedIndices.includes(this.currentWordIndex));
                 this.currentWord = this.words[this.currentWordIndex];
+                this.chineseInput = ''
                 this.romanjiInput = '';
                 this.meaning = ''
                 this.usedIndices.push(this.currentWordIndex);
             },
             checkInput() {
-                if (this.romanjiInput === this.currentWord.romanji && this.meaning === this.currentWord.vietnamese) {
+                if (this.chineseInput === this.currentWord.chinese && this.romanjiInput === this.currentWord.romanji && this.meaning === this.currentWord.vietnamese) {
                     this.remainWord = this.remainWord - 1
                     this.nextWord();
                     this.error = ''
-                    this.hint = ''
                 } else {
                     this.error = 'Sai'
                 }
             },
-            giveHint() {
-                this.hint = this.currentWord.vietnamese
-            }
         },
     };
     </script>
